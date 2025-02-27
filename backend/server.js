51c7729ff219a01bc1d211f7cde5cbca087bc977
Kota-Dev217 <kota31479@gmail.com>
2025-02-27 17:16:29 -0500

    Update mapex.fun

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,6 @@ const PORT = process.env.PORT || 8000;
 let server;
 connectDB().then(async () => {
   server = app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on port ${process.env.SERVER_URL || "http://127.0.0.1"}:${PORT}`);
+    console.log(`Server running on port ${process.env.SERVER_URL || "https://mapex.fun"}:${PORT}`);
   });
 });
